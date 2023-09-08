114eead3675ef06eb8fe36d97df6e4cad923d3bb
unknown <Kazachenko.danya@inbox.ru>
2023-09-08 14:45:42 +0300

    Initial commit

---
+++ src/Rocket3.java
@@ -0,0 +1,9 @@
+public class Rocket3 {
+    public static void main(String[] args) {
+        Rocket rocket = new Rocket("Rocket A", 1000.0, 2000.0, 1);
+        System.out.println("Name: " + rocket.getName());
+        System.out.println("Range: " + rocket.getRange() + " km");
+        System.out.println("Speed: " + rocket.getSpeed() + " m/s");
+        System.out.println("Warhead: " + rocket.getWarhead());
+    }
+}
