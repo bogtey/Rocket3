4a21509a16fcfd67223062ee1939de031d4cd8b5
bogtey <106816066+bogtey@users.noreply.github.com>
2023-09-20 11:10:22 +0300

    Update Rocket3.java

--- src/Rocket3.java
+++ src/Rocket3.java
@@ -1,4 +1,4 @@
-public class Rocket3 {
+class Rocket3 {
     public static void main(String[] args) {
         Rocket rocket = new Rocket("Rocket A", 1000.0, 2000.0, 1);
         System.out.println("Name: " + rocket.getName());
